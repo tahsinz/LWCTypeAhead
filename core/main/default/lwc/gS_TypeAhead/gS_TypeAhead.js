b204c90ca093ba30fcb620ab26e5bb31536cfba7
Tahsin Zulkarnine <tahsin@gscloudsolutions.com>
2019-04-26 16:57:12 -0700

    Added the  lightning web component with labels

--- core/main/default/lwc/gS_TypeAhead/gS_TypeAhead.js
+++ core/main/default/lwc/gS_TypeAhead/gS_TypeAhead.js
@@ -1,3 +1,11 @@
 import { LightningElement } from 'lwc';
+// Import custom labels
+import searchLabel from '@salesforce/label/c.Search_Label';
+import placeholderLabel from '@salesforce/label/c.Search_Place_Holder';
+export default class GS_TypeAhead extends LightningElement {
 
-export default class GS_TypeAhead extends LightningElement {}
+    label = {
+        searchLabel,
+        placeholderLabel,
+    };
+}

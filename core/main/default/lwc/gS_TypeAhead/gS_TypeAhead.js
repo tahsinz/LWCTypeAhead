5e4663e647ad26a067a5226850d97d88f02e6580
Tahsin Zulkarnine <tahsin@gscloudsolutions.com>
2019-04-28 23:07:47 -0700

    Integrated jQUery and Typeahead. Tried to feed in search results to typeahead

--- core/main/default/lwc/gS_TypeAhead/gS_TypeAhead.js
+++ core/main/default/lwc/gS_TypeAhead/gS_TypeAhead.js
@@ -4,13 +4,16 @@ import getSearchResults from '@salesforce/apex/GSLX_TypeAheadController.getSearc
 // Import custom labels
 import searchLabel from '@salesforce/label/c.Search_Label';
 import placeholderLabel from '@salesforce/label/c.Search_Place_Holder';
+//static resources
+import typeAheadResource from '@salesforce/resourceUrl/Typeahed';
+import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
 
 export default class GS_TypeAhead extends LightningElement {
 
     
     @api PlaceHolderText = "Search";
     @api MinLength = 3;
-    @api HighlightResult;
+    @api HighlightResult = false;
     @api SearchGroups = "ALL FIELDS";   
     @api  ReturningFields;
     @api LoadingHTML;
@@ -30,19 +33,120 @@ export default class GS_TypeAhead extends LightningElement {
     @track error;
     @track actualFields = [];
 
+    typeaheadInitialized = false;
+
+
     label = {
         searchLabel,
         placeholderLabel,
     };
-    goToSearchPage( event) {
-        console.log('clicked on search button');
-        this.handleSearch();
+
+    renderedCallback() {
+
+        if (this.typeaheadInitialized) {
+            return;
+        }
+        this.typeaheadInitialized = true;
+        this.initializeTypeAhead();
         
     }
 
-    handleSearch(){
-        this.searchTerm = 'Test';
-        this.searchGroup = 'All Fields';
+    goToSearchPage( event) {
+        console.log('clicked on search button');
+        let searchString = this.getSearchValue();
+        console.log(searchString);
+        this.handleSearch(searchString);
+        
+    }
+    initializeTypeAhead(){
+
+        // load required JS and css files
+        Promise.all([
+            loadScript(this, typeAheadResource+'/Typeahead/jquery-3.2.1.min.js'),
+            loadScript(this, typeAheadResource+'/Typeahead/DataCache.js'),
+            loadScript(this, typeAheadResource+'/Typeahead/typeahead.js'),
+            loadStyle(this, typeAheadResource + '/Typeahead/salesforce-lightning-design-system.min.css'),
+        ])
+        .then(() => { 
+            console.log('Loaded typeahead related resources');
+            // check the attribute degined in community
+            console.log('Checking configurd attributes');
+            console.log('Loading html: '+this.LoadingHTML);
+            console.log('No Result HTML: '+this.NoResultHTML);
+            console.log('Min Length:'+this.MinLength);
+            console.log('Highlight Result: '+this.HighlightResult);
+            // get the input element
+            const inputBox = this.template.querySelector('.gs-global-search');
+            if(inputBox === undefined)   console.log('No element found');
+            else console.log('Found input element. Placeholder used: '+inputBox.getAttribute('placeholder'));
+            //initialize jquery typeahead
+            this.configureTypeHead(inputBox);
+            
+            
+            
+        })
+        .error(error => { 
+            console.log('Found Error loading jquery:'+JSON.stringify(error))
+        });
+        
+        
+        
+
+    }
+    getSearchValue(){
+        const inputBox = this.template.querySelector('.gs-global-search');
+        if(inputBox === undefined)   {
+            console.log('No element found');
+            return null;
+        }
+        return inputBox.value; 
+    }
+    configureTypeHead(inputBox){
+        
+        //debugger;
+        $(inputBox).typeahead(
+            {
+                minLength: this.MinLength,
+                highlight: this.HighlightResult
+            },
+            {
+                name: 'searchResults',
+                limit: Infinity,
+                async: false,
+                templates: {
+                   pending :   this.LoadingHTML,
+                   notFound : this.NoResultHTML
+                },
+                source: function() {
+                      let searchTerm =   inputBox.value;
+                      this.handleSearch(searchTerm)
+                }
+                ,
+               
+                display: (item) => {
+                    
+                }
+                
+            }
+            
+        )
+       
+        //debugger;
+        console.log('finished');
+
+        return true;
+        
+    }
+
+    handleSearch(queryTerm){
+
+
+        this.searchTerm = queryTerm;
+        if(queryTerm === undefined )    {
+            console.log('Invalid search term provided.');
+            return;
+        }
+        if(this.searchGroup === undefined)   this.searchGroup = 'All Fields';
         console.log('Making Apex call ..');
 
         getSearchResults(
@@ -59,7 +163,7 @@ export default class GS_TypeAhead extends LightningElement {
         .then(result => {
             this.searchResults = result;
             this.error = undefined;
-            console.log(JSON.parse(JSON.stringify(this.searchResults)));
+            console.log('Response found:' +JSON.stringify(this.searchResults));
             
         })
         .catch(error => {
@@ -68,8 +172,5 @@ export default class GS_TypeAhead extends LightningElement {
         });
 
     }
-
-
-    
   
 }
---
+++ core/main/default/staticresources/Typeahed/Typeahead/DataCache.js
@@ -0,0 +1,13 @@
+window.DataCache = (function(){
+	var cache = {};
+	
+	return {
+		setData: function(name, data){
+			cache[name] = data;
+		},
+		
+		getData: function(name){
+			return cache[name];
+		}
+	};
+}());
